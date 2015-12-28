90ad54264562d16fb0defed8cadf2c46e2e3e1b8
Greg McGlynn <gem2128@columbia.edu>
2015-12-27 23:56:48 -0500

    basic bug and lookahead bug

--- teams/scraps/RobotPlayer.java
+++ teams/scraps/RobotPlayer.java
@@ -2,13 +2,10 @@ package scraps;
 
 import battlecode.common.*;
 
-public class RobotPlayer {
-	static MapLocation ourHQ;
-	static MapLocation theirHQ;
-	static RobotController rc;
+public class RobotPlayer extends Info {
 	static java.util.Random rand;
 	
-	public static void run(RobotController theRC) {
+	public static void run(RobotController theRC) throws GameActionException {
 		rc = theRC;
 		ourHQ = rc.senseHQLocation();
 		theirHQ = rc.senseEnemyHQLocation();
@@ -16,30 +13,33 @@ public class RobotPlayer {
 		FastMath.initRand(rc);
 		
 		while(true) {
-			runTests();
+			here = rc.getLocation();
+			switch(rc.getType()) {
+			case HQ:
+				turnHQ();
+				break;
+			case BEAVER:
+				turnBeaver();
+				break;
+			default:
+				break;
+			}
 			rc.yield();
 		}
 	}
-
-	static void runTests() {
-		int timer0 = Clock.getBytecodeNum();
-		int slowDist = (int)Math.sqrt(ourHQ.distanceSquaredTo(theirHQ));
-		int timer1 = Clock.getBytecodeNum();
-		int fastDist = FastMath.floorSqrt(ourHQ.distanceSquaredTo(theirHQ));
-		int timer2 = Clock.getBytecodeNum();
-		int slowRandomRand = rand.nextInt(256);
-		int timer3 = Clock.getBytecodeNum();
-		int slowMathRand = (int)(256 * Math.random());
-		int timer4 = Clock.getBytecodeNum();
-		int fastRand = FastMath.rand256();
-		int timer5 = Clock.getBytecodeNum();
-		
-		rc.setIndicatorString(0, String.format("slowDist = %d, fastDist = %d, slowDist time = %d, fastDistTime = %d",
-				slowDist, fastDist, timer1 - timer0, timer2 - timer1));
-		
-		rc.setIndicatorString(1, String.format("slowRandomRand = %d, slowMathRand = %d, fastRand = %d", 
-				slowRandomRand, slowMathRand, fastRand));
-		rc.setIndicatorString(2, String.format("slowRandomRand time = %d, slowMathRand time = %d, fastRand time = %d", 
-				timer3 - timer2, timer4 - timer3, timer5 - timer4));
+	
+	static void turnHQ() throws GameActionException {
+		if (!rc.isCoreReady()) return;
+		if (Clock.getRoundNum() % 2000 > 0) return;
+		for (Direction dir : Direction.values()) {
+			if (rc.canSpawn(dir, RobotType.BEAVER)) {
+				rc.spawn(dir, RobotType.BEAVER);
+				return;
+			}
+		}
+	}
+	
+	static void turnBeaver() throws GameActionException {
+		if (rc.isCoreReady()) LookaheadBug.goTo(theirHQ);
 	}
 }
