56eecbbe7ebb0738d8921534f251d720563895ed
Luchang Jin <waterret@gmail.com>
2016-01-04 21:21:22 -0500

    add first player

---
+++ src/first/RobotPlayer.java
@@ -0,0 +1,19 @@
+package examplefuncsplayer;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public class RobotPlayer {
+
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * If this method returns, the robot dies!
+     **/
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) {
+		while (true) {
+			Clock.yield();
+		}
+    }
+}
