329188d5466c7fc1184eb8b1eac2a484a42a265c
Greg McGlynn <gem2128@columbia.edu>
2016-01-04 22:35:06 -0500

    add Globals.here

--- src/first/Globals.java
+++ src/first/Globals.java
@@ -1,10 +1,12 @@
 package first;
 
+import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.Team;
 
 public class Globals {
 	public static RobotController rc;
+	public static MapLocation here;
 	public static Team us;
 	public static Team them;
 	
@@ -12,5 +14,9 @@ public class Globals {
 		rc = theRC;
 		us = rc.getTeam();
 		them = us.opponent();
+	}
+	
+	public static void update() {
+		here = rc.getLocation();
 	}
 }
