6fb7f390bd9d8efc4a3bb03ab3efb0396f13e1ef
Luchang Jin <waterret@gmail.com>
2016-01-06 21:12:18 -0500

    add sodier_rubble from sodier

---
+++ src/sodier_rubble/BotScout.java
@@ -0,0 +1,20 @@
+package sodier_rubble;
+
+import battlecode.common.Clock;
+
+public class BotScout {
+	public static void loop() {
+		while (true) {
+			try {
+			    turn();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Clock.yield();
+		}
+	}
+	
+	private static void turn() {
+		
+	}
+}
