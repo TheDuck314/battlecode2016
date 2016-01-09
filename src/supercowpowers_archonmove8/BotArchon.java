e5eb7be2f1ce199fac4beeab7e1837a06000443a
Luchang Jin <waterret@gmail.com>
2016-01-08 21:25:49 -0500

    add supercowpowers_archonmove8

---
+++ src/supercowpowers_archonmove8/BotArchon.java
@@ -0,0 +1,367 @@
+package supercowpowers_archonmove8;
+
+import battlecode.common.*;
+
+public class BotArchon extends Globals {
+	private static MapLocationHashSet knownZombieDens = new MapLocationHashSet();
+	private static MapLocation[] denAttackQueue = new MapLocation[1000];
+	private static int denAttackQueueHead = 0;
+	private static int denAttackQueueTail = 0;
+
+	private static int spawnCount = 0;
+
+	private static int lastArchonLocationMessageRound = 0;
+
+	private static int nArchons = 0;
+	private static MapLocation[] archonsLoc = new MapLocation[10];
+	private static int[] archonsId = new int[10];
+	private static int archonOrder = 0;
+	private static MapLocation rallyPoint = null;
+	
+	public static void loop() throws GameActionException {
+		Debug.init("heal");
+		FastMath.initRand(rc);
+		initArchons();
+
+		while (true) {
+			try {
+				Globals.update();
+			    turn();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Clock.yield();
+		}
+	}
+	
+	private static void initArchons() throws GameActionException {
+		Globals.update();
+		archonsLoc[nArchons] = here;
+		archonsId[nArchons] = myID;
+		nArchons += 1;
+		Messages.sendArchonLocation(here, MapEdges.maxBroadcastDistSq());
+		Clock.yield();
+		Signal[] signals = rc.emptySignalQueue();
+		for (Signal sig : signals) {
+			if (sig.getTeam() != us) continue;
+			int[] data = sig.getMessage();
+			if (data != null) {
+				if ((data[0] & Messages.CHANNEL_MASK) == Messages.CHANNEL_ARCHON_LOCATION) {
+					archonsLoc[nArchons] = sig.getLocation();
+					archonsId[nArchons] = sig.getID();
+					nArchons += 1;
+				}
+			}
+		}
+		rallyPoint = here;
+		for (int i = 1; i < nArchons; ++i) {
+			rallyPoint = FastMath.addVec(rallyPoint, archonsLoc[i]);
+		}
+		rallyPoint = FastMath.multiplyVec(1.0/(double)nArchons, rallyPoint);
+		rallyPoint = rallyPoint.add(rallyPoint.directionTo(here), 0);
+		rc.setIndicatorDot(rallyPoint, 255, 0, 0);
+		for (int i = 0; i < nArchons; ++i) {
+			if (archonsId[i] < myID) {
+				archonOrder += 1;
+			}
+		}
+	}
+	
+	private static void turn() throws GameActionException {
+		if (rc.getRoundNum() <= 100 && rallyPoint != null && here.distanceSquaredTo(rallyPoint) > 2) {
+			if (rc.isCoreReady()) {
+			    DBug.goTo(rallyPoint);
+			}
+			return;
+		}
+		
+		processSignals();
+		MapEdges.detectAndBroadcastMapEdges(5); // visionRange = 5
+
+		trySendAttackTarget();
+		
+		trySendArchonLocationMessage();
+
+		
+		trySpawn();
+		
+		tryRepairAlly();
+		tryConvertNeutrals();
+		
+		moveAround();
+		//exploreForNeutralsAndParts();	
+	}
+	
+	private static int nTurret = 0;
+	
+	public static MapLocation dangerousLoc = null;
+	public static int dangerousTurn = 0;
+	public static double myHealth;
+
+	public static void updateDangerousLoc() {
+		if (rc.getHealth() < myHealth) {
+			dangerousLoc = here;
+			dangerousTurn = 0;
+			myHealth = rc.getHealth();
+		} else if (dangerousLoc != null) {
+			if (dangerousTurn >= 200) {
+				dangerousLoc = null;
+				dangerousTurn = 0;
+			}
+			dangerousTurn += 1;
+		}
+	}
+
+	private static void moveAround() throws GameActionException {
+		updateDangerousLoc();
+		if (!rc.isCoreReady()) return;
+		Direction[] dirs = new Direction[9];
+		boolean[] cmoves = new boolean[9];
+		MapLocation[] locs = new MapLocation[9];
+		boolean[] oddPos = new boolean[9];
+		double[] rubbles = new double[9];
+		double[] attacks = new double[9];
+		double[] nturrets = new double[9];
+		double[] turrets = new double[9];
+		double[] scouts  = new double[9];
+		double[] archons  = new double[9];
+		dirs[8] = null;
+		cmoves[8] = true;
+		locs[8] = here;
+		for (int i = 0; i < 8; ++i) {
+			dirs[i] = Direction.values()[i];
+			locs[i] = here.add(dirs[i]);
+			cmoves[i] = rc.canMove(dirs[i]);
+		}
+		for (int i = 0; i < 9; ++i) {
+			oddPos[i] = (locs[i].x + locs[i].y) % 2 != 0;
+			rubbles[i] = rc.senseRubble(locs[i]);
+		}
+		RobotInfo[] infos;
+		infos = rc.senseHostileRobots(here, mySensorRadiusSquared);
+		for (RobotInfo e : infos) {
+			for (int i = 0; i < 9; ++i) {
+				if (e.location.distanceSquaredTo(locs[i]) <= e.type.attackRadiusSquared) {
+					attacks[i] += e.attackPower;
+				}
+			}
+		}
+		infos = rc.senseNearbyRobots(mySensorRadiusSquared, us);
+		MapLocation turretVec = new MapLocation(0,0);
+		MapLocation scoutVec = new MapLocation(0,0);
+		MapLocation archonVec = new MapLocation(0,0);
+		for (RobotInfo f : infos) {
+			if (f.ID == myID) {
+				continue;
+			}
+			switch (f.type) {
+			case ARCHON:
+				if (here.distanceSquaredTo(f.location) < 4) {
+					archonVec = archonVec.add(here.directionTo(f.location));
+				}
+				break;
+			case TURRET:
+				for (int i = 0; i < 9; ++i) {
+					if (f.location.distanceSquaredTo(locs[i]) < 9) {
+						nturrets[i] += 1;
+					}
+				}
+				turretVec = turretVec.add(here.directionTo(f.location));
+				break;
+			case SCOUT:
+				scoutVec = scoutVec.add(here.directionTo(f.location));
+				break;
+			default:
+			}
+		}
+//		rc.setIndicatorLine(here, FastMath.addVec(turretVec, FastMath.addVec(here, FastMath.multiplyVec(-5,scoutVec))), 0, 255, 0);
+		for (int i = 0; i < 9; ++i) {
+			turrets[i] = FastMath.dotVec(dirs[i], turretVec);
+			scouts[i] = FastMath.dotVec(dirs[i], scoutVec);
+			archons[i] = FastMath.dotVec(dirs[i], archonVec);
+		}
+		double[] scores = new double[9];
+		for (int i = 0; i < 9; ++i) {
+			scores[i] = -attacks[i] * 1000;
+			if (locs[i] == dangerousLoc) {
+				scores[i] -= 2000;
+			}
+			if (rubbles[i] >= GameConstants.RUBBLE_SLOW_THRESH) {
+				scores[i] -= attacks[8] * 1000;
+				scores[i] += 500;
+			}
+//			if (oddPos[i]) {
+//				scores[i] += 100;
+//			}
+//			scores[i] += nturrets[i] * 50;
+//			scores[i] += turrets[i] + scouts[i];
+//			scores[i] -= archons[i] * 50;
+		}
+		scores[8] += 500;
+		for (int i = 0; i < 8; ++i) {
+			if (rubbles[i] < GameConstants.RUBBLE_SLOW_THRESH && !cmoves[i]) {
+				scores[i] = -100000;
+			}
+		}
+		double bestScore = -100000;
+		Direction bestDir = null;
+		int bestI = 8;
+		int rdn = FastMath.rand256();
+		for (int i = 0; i < 9; ++i) {
+			if (bestScore < scores[(rdn + i) % 9]) {
+				bestDir = dirs[(rdn + i) % 9];
+				bestScore = scores[(rdn + i) % 9];
+				bestI = i;
+			}
+		}
+		nTurret = (int)nturrets[8];
+		if (bestDir != null) {
+			if (rc.canMove(bestDir)) {
+				nTurret = (int)nturrets[bestI];
+			}
+			DBug.tryMoveClearDir(bestDir);
+		} else if (rubbles[8] >= GameConstants.RUBBLE_SLOW_THRESH) {
+			rc.clearRubble(Direction.NONE);
+		}
+		return;
+	}
+	
+	private static void trySendArchonLocationMessage() throws GameActionException {
+		if (lastArchonLocationMessageRound < rc.getRoundNum() - 40) {
+			Messages.sendArchonLocation(here, MapEdges.maxBroadcastDistSq());
+			lastArchonLocationMessageRound = rc.getRoundNum();
+			Debug.indicate("heal", 0, "sent archon location");
+		}
+	}
+	
+	private static void trySendAttackTarget() throws GameActionException {
+		RobotInfo[] targets = rc.senseHostileRobots(here, mySensorRadiusSquared);
+		int numTargets = (targets.length < 3 ? targets.length : 3);
+		for (int i = 0; i < numTargets; ++i) {
+			Messages.sendAttackTarget(targets[i].location, 9 * mySensorRadiusSquared);
+		}
+	}
+	
+	private static void tryAttackZombieDen() throws GameActionException {
+		if (!rc.isCoreReady()) return;
+		
+		MapLocation target = denAttackQueue[denAttackQueueHead];
+		if (target == null) {
+			Debug.indicate("dens", 0, "target is null :(");
+			return;
+		}
+
+		if (rc.canSenseLocation(target)) {
+			RobotInfo botAtTarget = rc.senseRobotAtLocation(target);
+			if (botAtTarget == null || botAtTarget.type != RobotType.ZOMBIEDEN) {
+				// this den has been destroyed. Move on to the next one.
+				++denAttackQueueHead;
+				Debug.indicate("dens", 0, "den at " + target + " seems to be destroyed. now head = " + denAttackQueueHead);
+				return;
+			}
+		}
+		
+		if (here.distanceSquaredTo(target) <= 20) {
+			Debug.indicate("dens", 0, "stationed near den at " + target);
+			//Messages.sendAttackTarget(target, 100*mySensorRadiusSquared);
+			return;
+		}
+		
+		Debug.indicate("dens", 0, "going to den at " + target);
+		//Bug.goTo(target);
+		DirectNav.goTo(target);
+		//Messages.sendAttackTarget(target, 2*mySensorRadiusSquared);
+	}
+
+	private static void trySpawn() throws GameActionException {
+		if (!rc.isCoreReady()) return;
+
+		RobotType spawnType = (spawnCount % 10 == (2 + archonOrder) % 10 ? RobotType.SCOUT : RobotType.SOLDIER);
+
+		if (!rc.hasBuildRequirements(spawnType)) return;
+
+		Direction dir = Direction.values()[FastMath.rand256() % 8];
+		for (int i = 0; i < 8; ++i) {
+			if (rc.canBuild(dir, spawnType)) {
+				rc.build(dir, spawnType);
+				++spawnCount;
+				if (spawnType == RobotType.SCOUT) {
+					Messages.sendKnownMapEdges(2); // tell scout known map edges
+				}
+			}
+		}
+	}
+	
+	private static void tryRepairAlly() throws GameActionException {
+		RobotInfo[] healableAllies = rc.senseNearbyRobots(RobotType.ARCHON.attackRadiusSquared, us);
+		MapLocation bestLoc = null;
+		double mostHealth = 0;
+		for (RobotInfo ally : healableAllies) {
+			if (ally.type == RobotType.ARCHON) continue;
+			if (ally.health < ally.maxHealth && ally.health > mostHealth) {
+				bestLoc = ally.location;
+				mostHealth = ally.health;
+			}
+		}
+		if (bestLoc != null) {
+			rc.repair(bestLoc);
+		}
+	}
+	
+	private static void tryConvertNeutrals() throws GameActionException {
+		if (!rc.isCoreReady()) return;
+		RobotInfo[] adjacentNeutrals = rc.senseNearbyRobots(2, Team.NEUTRAL);
+		for (RobotInfo neutral : adjacentNeutrals) {
+			rc.activate(neutral.location);
+			return;
+		}
+	}
+	
+	
+	private static void processSignals() {
+		Signal[] signals = rc.emptySignalQueue();
+		for (Signal sig : signals) {
+			if (sig.getTeam() != us) continue;
+			
+			int[] data = sig.getMessage();
+			if (data != null) {
+				switch(data[0] & Messages.CHANNEL_MASK) {
+				case Messages.CHANNEL_FOUND_PARTS:
+					PartsLocation partsLoc = Messages.parsePartsLocation(data);
+					Debug.indicate("explore", 0, "heard about " + partsLoc.numParts + " parts at " + partsLoc.location);
+					break;
+				case Messages.CHANNEL_ZOMBIE_DEN:
+					MapLocation zombieDenLoc = Messages.parseZombieDenLocation(data);
+					Debug.indicate("explore", 1, "heard about a zombie den at " + zombieDenLoc);
+					if (knownZombieDens.add(zombieDenLoc)) {
+						denAttackQueue[denAttackQueueTail++] = zombieDenLoc;
+					} else {
+						Debug.indicateAppend("explore", 1, "; but we already knew about it");
+					}
+					break;
+				case Messages.CHANNEL_ENEMY_TURRET_WARNING:
+					MapLocation enemyTurretLoc = Messages.parseEnemyTurretWarning(data);
+					Debug.indicate("explore", 2, "heard about an enemy turret at " + enemyTurretLoc);
+					break;
+					
+				case Messages.CHANNEL_MAP_MIN_X:
+					Messages.processMapMinX(data);
+					break;
+				case Messages.CHANNEL_MAP_MAX_X:
+					Messages.processMapMaxX(data);
+					break;
+				case Messages.CHANNEL_MAP_MIN_Y:
+					Messages.processMapMinY(data);
+					break;
+				case Messages.CHANNEL_MAP_MAX_Y:
+					Messages.processMapMaxY(data);
+					break;
+					
+				default:
+				}
+			} else {
+				// simple signal with no message
+			}
+		}
+	}
+}
---
+++ src/supercowpowers_archonmove8/BotScout.java
@@ -0,0 +1,324 @@
+package supercowpowers_archonmove8;
+
+import battlecode.common.*;
+
+public class BotScout extends Globals {
+	private static MapLocation origin;
+	private static boolean[][] exploredGrid = new boolean[100][100];
+	private static MapLocation exploreDest = null;
+	private static int exploreDestGridX;
+	private static int exploreDestGridY;
+	private static boolean finishedExploring = false;
+	private static final int gridSpacing = 10;
+	private static final int DANGER_MEMORY_LENGTH = 5;
+	private static MapLocation[] dangerMemory = new MapLocation[DANGER_MEMORY_LENGTH];
+	private static int dangerMemoryPointer = 0;
+
+	public static void loop() {
+    	Debug.init("safebug");
+    	origin = here;
+    	exploredGrid[50][50] = true;    	
+		while (true) {
+			try {
+				Globals.update();
+			    turn();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Clock.yield();
+		}
+	}
+	
+	private static void turn() throws GameActionException {
+		processSignals();		
+		MapEdges.detectAndBroadcastMapEdges(7); // visionRange = 7
+		
+		retreatIfNecessary();
+		
+		trySendAttackTarget();
+		
+		explore();
+	}
+	
+	private static void trySendAttackTarget() throws GameActionException {
+		RobotInfo[] targets = rc.senseHostileRobots(here, mySensorRadiusSquared);
+		int numSent = 0;
+		for (RobotInfo target : targets) {
+			Messages.sendAttackTarget(target.location, 9 * mySensorRadiusSquared);
+			++numSent;
+			if (numSent >= 3) return;
+		}
+	}
+	
+
+	private static void processSignals() {
+		Signal[] signals = rc.emptySignalQueue();
+		for (Signal sig : signals) {
+			if (sig.getTeam() != us) continue;
+			
+			int[] data = sig.getMessage();
+			if (data != null) {
+				switch(data[0] & Messages.CHANNEL_MASK) {
+				case Messages.CHANNEL_MAP_MIN_X:
+					Messages.processMapMinX(data);
+					break;
+				case Messages.CHANNEL_MAP_MAX_X:
+					Messages.processMapMaxX(data);
+					break;
+				case Messages.CHANNEL_MAP_MIN_Y:
+					Messages.processMapMinY(data);
+					break;
+				case Messages.CHANNEL_MAP_MAX_Y:
+					Messages.processMapMaxY(data);
+					break;
+					
+				default:
+				}
+			}
+		}
+	}
+	
+	
+	
+	private static MapLocation gridLocation(int gridX, int gridY) {
+		return new MapLocation(origin.x + gridSpacing*(gridX-50), origin.y + gridSpacing*(gridY-50));
+	}
+	
+	private static int nearestGridX(MapLocation loc) {
+		return 50 + (int)Math.round((here.x - origin.x)/((double)gridSpacing));
+	}
+	
+	private static int nearestGridY(MapLocation loc) {
+		return 50 + (int)Math.round((here.y - origin.y)/((double)gridSpacing));
+	}
+	
+	private static int[] legDX = { 0, 0, 0, -1, 0, 0, 0, 1 };
+    private static int[] legDY = { 0, 1, 0, 0, 0, -1, 0, 0 };
+    
+    private static boolean isFarOffMap(MapLocation loc) {
+    	return (MapEdges.minX != MapEdges.UNKNOWN && loc.x < MapEdges.minX - gridSpacing/2)
+				|| (MapEdges.maxX != MapEdges.UNKNOWN && loc.x > MapEdges.maxX + gridSpacing/2) 
+				|| (MapEdges.minY != MapEdges.UNKNOWN && loc.y < MapEdges.minY - gridSpacing/2) 
+				|| (MapEdges.maxY != MapEdges.UNKNOWN && loc.y > MapEdges.maxY + gridSpacing/2);
+    }
+    
+    private static MapLocation moveOntoMap(MapLocation loc) {
+    	int retX = loc.x;
+    	int retY = loc.y;
+    	if (MapEdges.minX != MapEdges.UNKNOWN && retX < MapEdges.minX) {
+    		retX = MapEdges.minX;
+    	}
+    	if (MapEdges.maxX != MapEdges.UNKNOWN && retX > MapEdges.maxX) {
+    		retX = MapEdges.maxX;
+    	}
+    	if (MapEdges.minY != MapEdges.UNKNOWN && retY < MapEdges.minY) {
+    		retY = MapEdges.minY;
+    	}
+    	if (MapEdges.maxY != MapEdges.UNKNOWN && retY > MapEdges.maxY) {
+    		retY = MapEdges.maxY;
+    	}
+    	return new MapLocation(retX, retY);
+    }
+    
+	private static void pickNewExploreDest() {
+		int centerX = nearestGridX(here);
+		int centerY = nearestGridY(here);
+		if (!exploredGrid[centerX][centerY]) {
+			MapLocation centerLoc = gridLocation(centerX, centerY);
+			if (!isFarOffMap(centerLoc)) {
+				exploreDest = moveOntoMap(centerLoc);
+				exploreDestGridX = centerX;
+				exploreDestGridY = centerY;
+				return;
+			}
+		}
+		
+        Direction startDiag = here.directionTo(origin);
+        if (startDiag == Direction.NONE || startDiag == Direction.OMNI) startDiag = Direction.NORTH_EAST;
+        if (!startDiag.isDiagonal()) startDiag = startDiag.rotateLeft();
+        
+		for (int radius = 1; radius <= 10; radius++) {
+            MapLocation bestLoc = null;
+            int bestGridX = -1;
+            int bestGridY = -1;
+            int bestDistSq = 999999;
+
+            int gridX = centerX + radius * startDiag.dx;
+            int gridY = centerY + radius * startDiag.dy;
+            int diag = startDiag.ordinal();
+            for (int leg = 0; leg < 4; leg++) {
+                for (int i = 0; i < 2 * radius; i++) {
+                	if (!exploredGrid[gridX][gridY]) {
+            			MapLocation gridLoc = gridLocation(gridX, gridY);
+            			if (!isFarOffMap(gridLoc)) {
+            				gridLoc = moveOntoMap(gridLoc);
+            				rc.setIndicatorDot(gridLoc, 0, 0, 255);
+            				int distSq = origin.distanceSquaredTo(gridLoc);
+                            if (distSq < bestDistSq) {
+                                bestDistSq = distSq;
+                                bestLoc = gridLoc;
+                                bestGridX = gridX;
+                                bestGridY = gridY;
+                            }
+                        }
+                    }
+
+                    gridX += legDX[diag];
+                    gridY += legDY[diag];
+                }
+
+                diag = (diag + 2) % 8;
+            }
+
+            if (bestLoc != null) {
+            	exploreDest = bestLoc;
+            	exploreDestGridX = bestGridX;
+            	exploreDestGridY = bestGridY;
+				rc.setIndicatorDot(exploreDest, 0, 255, 0);
+				return;
+            }
+        }
+		
+		exploreDest = null;
+		finishedExploring = true;
+	}
+	
+	
+	
+	private static boolean[] checkWhichSquaresAreSafe() {
+		boolean[] isSquareSafe = new boolean[8];
+		
+		Direction[] dirs = Direction.values();		
+		MapLocation[] dirLocs = new MapLocation[8];
+		for (int d = 0; d < 8; ++d) {
+			Direction dir = dirs[d];
+			isSquareSafe[d] = rc.canMove(dir);
+			dirLocs[d] = here.add(dir);
+			if (isSquareSafe[d]) {
+				rc.setIndicatorDot(dirLocs[d], 0, 255, 0);
+			} else {
+				rc.setIndicatorDot(dirLocs[d], 255, 0, 0);
+			}
+			if (isSquareSafe[d]) {
+				for (int i = 0; i < DANGER_MEMORY_LENGTH; ++i) {
+					if (dirLocs[d].equals(dangerMemory[i])) {
+						isSquareSafe[d] = false;
+						break;
+					}
+				}
+			}
+		}
+
+		RobotInfo[] hostiles = rc.senseHostileRobots(here, mySensorRadiusSquared);
+		Debug.indicate("safebug", 2, "hostiles.length = " + hostiles.length);
+		for (RobotInfo hostile : hostiles) {
+			RobotType hostileType = hostile.type;
+			if (!hostileType.canAttack()) {
+				continue;				
+			}
+			int hostileRangeSq = Math.max(hostileType.attackRadiusSquared, 8);
+			MapLocation hostileLoc = hostile.location;
+			for (int d = 0; d < 8; ++d) {
+				if (isSquareSafe[d]) {
+					if (dirLocs[d].distanceSquaredTo(hostileLoc) <= hostileRangeSq) {
+						isSquareSafe[d] = false;
+						rc.setIndicatorDot(dirLocs[d], 255, 0, 0);
+					}
+				}
+			}
+		}
+		
+		return isSquareSafe;
+	}
+	
+	private static boolean retreatIfNecessary() throws GameActionException {
+		if (!rc.isCoreReady()) return false;
+		
+		RobotInfo[] hostiles = rc.senseHostileRobots(here, mySensorRadiusSquared);
+		boolean mustRetreat = false;
+		for (RobotInfo hostile : hostiles) {
+			RobotType hostileType = hostile.type;
+			if (!hostileType.canAttack()) {
+				continue;				
+			}
+			int distSq = here.distanceSquaredTo(hostile.location);
+			if (distSq <= Math.max(hostileType.attackRadiusSquared, 8)) {
+				mustRetreat = true;
+				if (hostileType == RobotType.TURRET && distSq >= 36) {
+					Debug.indicate("safebug", 1, "dangerMemory[" + dangerMemoryPointer + "] = " + here);
+					dangerMemory[dangerMemoryPointer] = here;
+					dangerMemoryPointer = (dangerMemoryPointer + 1) % DANGER_MEMORY_LENGTH;
+				}
+				break;
+			}
+		}
+		
+		if (mustRetreat) {
+			Direction[] dirs = Direction.values();		
+			MapLocation[] dirLocs = new MapLocation[8];
+			int[] penalties = new int[8];
+			boolean[] canMove = new boolean[8];
+			for (int d = 0; d < 8; ++d) {
+				dirLocs[d] = here.add(dirs[d]);
+				canMove[d] = rc.canMove(dirs[d]);
+			}
+			
+			for (RobotInfo hostile : hostiles) {
+				RobotType hostileType = hostile.type;
+				if (!hostileType.canAttack()) {
+					continue;				
+				}
+				MapLocation hostileLoc = hostile.location;
+				int hostileRange = Math.max(hostileType.attackRadiusSquared, 8);
+				for (int d = 0; d < 8; ++d) {
+					if (canMove[d] && hostileLoc.distanceSquaredTo(dirLocs[d]) <= hostileRange) {
+						penalties[d]++;
+					}
+				}
+			}
+
+			Direction bestDir = null;
+			int minPenalty = 999999;
+			for (int d = 0; d < 8; ++d) {
+				if (canMove[d] && penalties[d] < minPenalty) {
+					minPenalty = penalties[d];
+					bestDir = dirs[d];
+				}
+			}
+			if (bestDir != null) {
+				rc.move(bestDir);
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private static void explore() throws GameActionException {
+		if (finishedExploring) return;
+		
+		Debug.indicate("explore", 0, "exploreDest = " + exploreDest);
+			
+		if (exploreDest != null && 
+				(here.equals(exploreDest) || 
+				here.isAdjacentTo(exploreDest) && rc.senseRobotAtLocation(exploreDest) != null)) {
+			// we reached the exploreDest
+			exploredGrid[exploreDestGridX][exploreDestGridY] = true;
+			//Debug.indicate("explore", 2, String.format("set exploredGrid[%d][%d] = true", exploreDestGridX, exploreDestGridY));
+			exploreDest = null; // pick a new explore dest
+		}
+		
+		if (exploreDest == null || MapEdges.isOffMap(exploreDest)) {
+			pickNewExploreDest();
+			Debug.indicate("explore", 1, "picked new exploreDest = " + exploreDest);
+			return;
+		}
+
+		
+		rc.setIndicatorDot(exploreDest, 0, 255, 0);
+		if (rc.isCoreReady()) {
+			Debug.indicate("explore", 1, "going to exploreDest");
+			ScoutBug.goTo(exploreDest, checkWhichSquaresAreSafe());
+		}
+	}
+}
