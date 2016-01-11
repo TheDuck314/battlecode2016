51a480f9054ce09475c209e30c8318f0191806f8
Greg McGlynn <gem2128@columbia.edu>
2016-01-11 01:24:30 -0500

    radar_pair_turretmemory10: archons remember turrets, turrets have improved targeting

---
+++ src/radar_pair_turretmemory10/FastTurretInfo.java
@@ -0,0 +1,13 @@
+package radar_pair_turretmemory10;
+
+import battlecode.common.*;
+
+public class FastTurretInfo {
+	public final int ID;
+	public final MapLocation location;
+	
+	public FastTurretInfo(int ID, MapLocation location) {
+		this.ID = ID;
+		this.location = location;
+	}
+}
---
+++ src/radar_pair_turretmemory10/Radar.java
@@ -0,0 +1,58 @@
+package radar_pair_turretmemory10;
+
+import battlecode.common.*;
+
+public class Radar extends Globals {	
+	public static MapLocation[] enemyTurretLocationById = new MapLocation[32001];
+	public static boolean[] haveSeenTurretId = new boolean[32001];
+	public static int[] enemyTurretIds = new int[1000];
+	public static int numEnemyTurrets = 0;
+	
+	public static void addEnemyTurret(int id, MapLocation loc) {
+		if (!haveSeenTurretId[id]) {
+			enemyTurretIds[numEnemyTurrets++] = id;
+			haveSeenTurretId[id] = true;
+		}
+		enemyTurretLocationById[id] = loc;
+	}
+	
+	public static void removeEnemyTurret(int id) {
+		enemyTurretLocationById[id] = null;
+	}
+	
+	public static FastTurretInfo findClosestEnemyTurret() {
+		FastTurretInfo ret = null;
+		int bestDistSq = Integer.MAX_VALUE;
+		for (int i = 0; i < numEnemyTurrets; ++i) {
+			int turretId = enemyTurretIds[i];
+			MapLocation turretLoc = enemyTurretLocationById[turretId];
+			if (turretLoc != null) {
+				int distSq = here.distanceSquaredTo(turretLoc);
+				if (distSq < bestDistSq) {
+					bestDistSq = distSq;
+					ret = new FastTurretInfo(turretId, turretLoc);
+				}
+			}
+		}
+		return ret;
+	}
+	
+	public static boolean turretIsKnown(int id, MapLocation loc) {
+		MapLocation storedLoc = enemyTurretLocationById[id];
+		if (storedLoc == null) return false;
+		return storedLoc.equals(loc);
+	}
+	
+	
+	public static FastRobotInfo[] enemyCache = new FastRobotInfo[1000];
+	public static int numCachedEnemies = 0;
+	
+	public static void addEnemyToCache(FastRobotInfo info) {
+		enemyCache[numCachedEnemies++] = info;
+	}
+	
+	public static void clearEnemyCache() {
+		enemyCache = new FastRobotInfo[1000];
+		numCachedEnemies = 0;
+	}
+}
