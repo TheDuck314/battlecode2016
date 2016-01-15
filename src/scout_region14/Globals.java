998ff7df98737fb5e0144a36fee2d6c867d10184
Luchang Jin <waterret@gmail.com>
2016-01-14 19:53:33 -0500

    scout_region14: from postsprint_13

---
+++ src/scout_region14/Globals.java
@@ -0,0 +1,34 @@
+package scout_region14;
+
+import battlecode.common.*;
+
+public class Globals {
+	public static RobotController rc;
+	public static MapLocation here;
+	public static Team us;
+	public static Team them;
+	public static int myID;
+	public static RobotType myType;
+	public static int myAttackRadiusSquared;
+	public static int mySensorRadiusSquared;
+	
+	public static void init(RobotController theRC) {
+		rc = theRC;
+		us = rc.getTeam();
+		them = us.opponent();
+		myID = rc.getID();
+		myType = rc.getType();
+		myAttackRadiusSquared = myType.attackRadiusSquared;
+		mySensorRadiusSquared = myType.sensorRadiusSquared;
+		here = rc.getLocation();
+	}
+	
+	public static void update() {
+		here = rc.getLocation();
+	}
+	
+	public static final int checkUnpairedScoutInterval = 50;
+	
+	public static RobotInfo[] visibleHostiles;
+	public static RobotInfo[] visibleAllies;
+}
