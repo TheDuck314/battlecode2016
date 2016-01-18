63a222f89fc645ee4c3235fe31879c5327810a54
Luchang Jin <waterret@gmail.com>
2016-01-18 02:16:28 -0500

    soldier_turret_retreat17: add missing file

---
+++ src/soldier_turret_retreat17/BigRobotInfo.java
@@ -0,0 +1,19 @@
+package soldier_turret_retreat17;
+
+import battlecode.common.*;
+
+public class BigRobotInfo {
+	public final int id;
+	public final MapLocation location;
+	public final RobotType type;
+	public final Team team;
+	public final int round;
+	
+	public BigRobotInfo(int id, MapLocation location, RobotType type, Team team, int round) {
+		this.id = id;
+		this.location = location;
+		this.type = type;
+		this.team = team;
+		this.round = round;
+	}
+}
