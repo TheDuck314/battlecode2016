5b552ea0493467709b6a75dae57867c72213a5c7
Greg McGlynn <gem2128@columbia.edu>
2016-01-18 21:32:37 -0500

    seeding_base18: copied from soldier_turret_retreat17

---
+++ src/seeding_base18/BigRobotInfo.java
@@ -0,0 +1,19 @@
+package seeding_base18;
+
+import battlecode.common.*;
+
+public class BigRobotInfo {
+	public final int id;
+	public final MapLocation location;
+	public final RobotType type;
+	public final Team team;
+	public final int round;
+	
+	public BigRobotInfo(int id, MapLocation location, RobotType type, Team team, int round) {
+		this.id = id;
+		this.location = location;
+		this.type = type;
+		this.team = team;
+		this.round = round;
+	}
+}
