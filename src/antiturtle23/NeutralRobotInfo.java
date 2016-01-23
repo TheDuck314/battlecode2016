20430765831e35464ea7d52d1b770c9fa53d4929
Greg McGlynn <gem2128@columbia.edu>
2016-01-23 13:33:57 -0500

    antiturtle23: copied from priorities22

---
+++ src/antiturtle23/NeutralRobotInfo.java
@@ -0,0 +1,13 @@
+package antiturtle23;
+
+import battlecode.common.*;
+
+public class NeutralRobotInfo {
+	public MapLocation location;
+	public RobotType type;
+	
+	public NeutralRobotInfo(MapLocation location, RobotType type) {
+		this.location = location;
+		this.type = type;
+	}
+}
