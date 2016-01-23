e3001076d4abf20b9387fb867e1c83b732263497
Greg McGlynn <gem2128@columbia.edu>
2016-01-23 16:43:41 -0500

    antiturtle23: prefer to heal turrets > vipers > others

--- src/antiturtle23/BotArchon.java
+++ src/antiturtle23/BotArchon.java
@@ -349,6 +349,9 @@ public class BotArchon extends Globals {
 		case TURRET: 
 			return 1000000.0 + ally.health;
 			
+		case VIPER:
+			return 10000.0 + ally.health;
+			
 		default:
 			return ally.health;
 		}
