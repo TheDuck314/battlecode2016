286a6b72b4cfa1918add1e7de5d176a6597efc9f
Luchang Jin <waterret@gmail.com>
2016-01-26 00:03:35 -0500

    final_25: from final_base_25

---
+++ src/final_25/PartMemory.java
@@ -0,0 +1,27 @@
+package final_25;
+
+import battlecode.common.*;
+
+public class PartMemory extends Globals {
+	public static final int MEMORY_LENGTH = 10;
+	public static PartRegion[] regions = new PartRegion[MEMORY_LENGTH];
+	static int pointer = 0;
+	
+	public static boolean add(PartRegion newRegion) {
+		for (int i = 0; i < MEMORY_LENGTH; ++i) {
+			PartRegion oldRegion = regions[i];
+			if (oldRegion != null 
+					&& oldRegion.centralLocation.distanceSquaredTo(newRegion.centralLocation) <= 35) {
+				return false; // already stored
+			}
+		}		
+		
+		regions[pointer] = newRegion;
+		pointer = (pointer + 1) % MEMORY_LENGTH;
+		return true;
+	}
+	
+	public static void remove(int index) {
+		regions[index] = null;
+	}
+}
